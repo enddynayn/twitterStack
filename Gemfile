d98243c54d8d517067a1c96f102855e526471235
endynayn <enddynayn@hotmail.com>
2017-06-05 22:41:31 -0700

    chore(*): add env file

--- Gemfile
+++ Gemfile
@@ -31,9 +31,11 @@ gem 'bcrypt', '~> 3.1.7'
 # gem 'rack-cors'
 
 group :development, :test do
+  gem 'dotenv-rails'
 end
 
 group :development do
+  gem 'dotenv-rails'
   gem 'listen', '~> 3.0.5'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- config/settings.rb
+++ config/settings.rb
@@ -2,13 +2,13 @@ class Settings
   extend Mixlib::Config
 
   config_context :mongoid do
-    default(:uri, 'mongodb://localhost:27017/stack_commerce')
+    default(:uri, ENV['MONGO_URI'])
   end
 
   config_context :twitter do
-    default(:consumer_key, 'FZAAPji7xCyHcNbLJVkudj3ul')
-    default(:consumer_secret, 'VRyCaUFK1t7RnvCRc4MlVy7GuNlRTWTUnja1KmQ0rxcxrBUtXg')
-    default(:access_token, '995884075-4A67GbYzlmqk82tzLAVMWIteZ3U6ZHpgQwLq5lns')
-    default(:access_token_secret, 'QpWhw7N46PViD6CzFS60drnV8KaP5knXiLIFtn1yXaSSl')
+    default(:consumer_key, ENV['TWITTER_CONSUMER_KEY'])
+    default(:consumer_secret, ENV['TWITTER_CONSUMER_SECRET'])
+    default(:access_token, ENV['TWITTER_ACCESS_TOKEN'])
+    default(:access_token_secret, ENV['TWITTER_TOKEN_SECRET'])
   end
 end
